2dcb324bfd728f41c8bb04880a1ada7e952acf84
MInesGMendes <20211624@novaims.unl.pt>
2025-02-09 21:17:31 +0000

    Update Chatbot.py

--- SecureShield/app_pages/Chatbot.py
+++ SecureShield/app_pages/Chatbot.py
@@ -1,7 +1,7 @@
 import streamlit as st
 import time
 from SecureShield.Chatbot.bot import MainChatbot  # Import the chatbot class
-from memory import MemoryManager
+from Chains.router.memory import MemoryManager
 import sqlitecloud  
 
 
