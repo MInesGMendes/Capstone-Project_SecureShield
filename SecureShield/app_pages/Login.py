6bcfc3fed00b76fbbd8318b6cac737994b36bfb9
MInesGMendes <20211624@novaims.unl.pt>
2025-02-09 20:46:02 +0000

    Update Login.py

--- SecureShield/app_pages/Login.py
+++ SecureShield/app_pages/Login.py
@@ -3,33 +3,25 @@ import streamlit as st
 import time
 
 # Database setup
-db_path = "sqlitecloud://cikwryuhhz.g6.sqlite.cloud:8860/shelfmate.db?apikey=fxZSlHTpxWU109u7go8avF2RRHsZ43JUxDfarjdFpYg"
-conn = sqlitecloud.connect(db_path)
+conn = sqlite3.connect("secure_shield.db")  # Path to your SQLite database
 cursor = conn.cursor()
 
 # Helper Functions
 def check_if_email_exists(email):
     cursor = conn.cursor()
-    cursor.execute("SELECT email FROM users WHERE email = ?", (email,))
+    cursor.execute("SELECT email FROM Employees WHERE email = ?", (email,))
     return cursor.fetchone() is not None
 
 def verify_user(email, password):
     cursor = conn.cursor()
-    cursor.execute("SELECT * FROM users WHERE email = ? AND password = ?", (email, password))
+    cursor.execute("SELECT * FROM Employees WHERE email = ? AND password = ?", (email, password))
     return cursor.fetchone()
 
 def get_username_by_email(email):
     cursor = conn.cursor()
-    cursor.execute("SELECT username FROM users WHERE email = ?", (email,))
+    cursor.execute("SELECT first_name FROM Employees WHERE email = ?", (email,))
     result = cursor.fetchone()
     return result[0] if result else None
-
-# Function to update user conversation_id in the database
-def update_user_conversation_id(username):
-    cursor = conn.cursor()
-    cursor.execute("UPDATE users SET conversation_id = conversation_id + 1 WHERE username = ?", (username,))
-    conn.commit()
-    conn.close()
 
 # Login Form
 st.title("Login")
@@ -59,11 +51,8 @@ with st.form("login_form"):
                 st.success(f"Welcome, {username}!")
                 update_user_conversation_id(username)
                 time.sleep(1)
-                st.switch_page("pages/Chatbot.py")
+                st.switch_page("app_pages/Chatbot.py")
             else:
                 st.error("Incorrect password")
                 st.session_state.logged_in = False
 
-st.write("Don't have an account yet? Register now!")
-if st.button("Register"):
-    st.switch_page("pages/Register.py")
